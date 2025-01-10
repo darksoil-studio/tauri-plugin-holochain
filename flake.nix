ba9752af31b39cf69212b3be34bd656b68595773
guillem.cordoba <guillem.cordoba@gmail.com>
2025-01-10 12:59:21 +0100

    Added cmake flags

--- flake.nix
+++ flake.nix
@@ -444,6 +444,28 @@
             inputs.tnesh-stack.outputs.dependencies.${system}.holochain.buildInputs;
 
           shellHook = ''
+            export BINDGEN_EXTRA_CLANG_ARGS=" \
+              $(< ${pkgs.stdenv.cc}/nix-support/libc-crt1-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/libc-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/cc-cflags) \
+              $(< ${pkgs.stdenv.cc}/nix-support/libcxx-cxxflags) \
+              ${
+                pkgs.lib.optionalString pkgs.stdenv.cc.isClang
+                " \n                -idirafter ${pkgs.stdenv.cc.cc}/lib/clang/${
+                                  pkgs.lib.getVersion pkgs.stdenv.cc.cc
+                                }/include"
+              } \
+              ${
+                pkgs.lib.optionalString pkgs.stdenv.cc.isGNU
+                " \n                -isystem ${pkgs.glibc_multi.dev}/include \n                -isystem ${pkgs.stdenv.cc.cc}/include/c++/${
+                                  pkgs.lib.getVersion pkgs.stdenv.cc.cc
+                                } \n                -isystem ${pkgs.stdenv.cc.cc}/include/c++/${
+                                  pkgs.lib.getVersion pkgs.stdenv.cc.cc
+                                }/${pkgs.stdenv.hostPlatform.config} \n                -idirafter ${pkgs.stdenv.cc.cc}/lib/gcc/${pkgs.stdenv.hostPlatform.config}/${
+                                  pkgs.pkgs.lib.getVersion pkgs.stdenv.cc.cc
+                                }/include"
+              }";
+
             export PS1='\[\033[1;34m\][p2p-shipyard-android:\w]\$\[\033[0m\] '
           '';
         };
