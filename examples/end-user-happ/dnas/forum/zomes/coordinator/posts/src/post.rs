b3a4247a9450b6816a6324b975312ac6862593c5
guillem.cordoba <guillem.cordoba@gmail.com>
2025-01-17 16:40:27 +0100

    Added log

--- examples/end-user-happ/dnas/forum/zomes/coordinator/posts/src/post.rs
+++ examples/end-user-happ/dnas/forum/zomes/coordinator/posts/src/post.rs
@@ -2,6 +2,8 @@ use hdk::prelude::*;
 use posts_integrity::*;
 #[hdk_extern]
 pub fn create_post(post: Post) -> ExternResult<Record> {
+    let start = sys_time()?;
+
     let post_hash = create_entry(&EntryTypes::Post(post.clone()))?;
     let record = get(post_hash.clone(), GetOptions::default())?.ok_or(wasm_error!(
         WasmErrorInner::Guest(String::from("Could not find the newly created Post"))
@@ -13,6 +15,8 @@ pub fn create_post(post: Post) -> ExternResult<Record> {
         LinkTypes::AllPosts,
         (),
     )?;
+    let end = sys_time()?;
+    error!("Elapsed: {:?}", start- end);
     Ok(record)
 }
 #[hdk_extern]
