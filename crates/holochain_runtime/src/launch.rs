9f7924a597bcd11730f65d909f4d4fb16ed10460
guillem.cordoba <guillem.cordoba@gmail.com>
2025-06-19 11:00:47 +0200

    Log default

--- crates/holochain_runtime/src/launch.rs
+++ crates/holochain_runtime/src/launch.rs
@@ -33,7 +33,8 @@ pub(crate) async fn launch_holochain_runtime(
     // if let Some(info) = lock.to_owned() {
     //     return Ok(info);
     // }
-    let crypto_provider = rustls::crypto::ring::default_provider().install_default();
+    log::error!("{:?}", rustls::crypto::CryptoProvider::get_default());
+    let crypto_provider = rustls::crypto::aws_lc_rs::default_provider().install_default();
     if crypto_provider.is_err() {
         log::error!(
             "could not set crypto provider for tls: {:?}.",
