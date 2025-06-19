8966c1cf5c16c73d9ccd7d46dc71033828e5c567
guillem.cordoba <guillem.cordoba@gmail.com>
2025-06-19 10:23:58 +0200

    Log error

--- crates/holochain_runtime/src/launch.rs
+++ crates/holochain_runtime/src/launch.rs
@@ -33,11 +33,12 @@ pub(crate) async fn launch_holochain_runtime(
     // if let Some(info) = lock.to_owned() {
     //     return Ok(info);
     // }
-    if rustls::crypto::aws_lc_rs::default_provider()
-        .install_default()
-        .is_err()
-    {
-        log::error!("could not set crypto provider for tls");
+    let crypto_provider = rustls::crypto::aws_lc_rs::default_provider().install_default();
+    if crypto_provider.is_err() {
+        log::error!(
+            "could not set crypto provider for tls: {:?}.",
+            crypto_provider
+        );
     }
 
     let filesystem = FileSystem::new(config.holochain_dir).await?;
