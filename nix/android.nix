c2a24cf2f42b0c115c502082ee126055c0420b35
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-08 13:59:30 +0200

    Attempt include

--- nix/android.nix
+++ nix/android.nix
@@ -60,7 +60,7 @@
         export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER=${toolchainBinsPath}/aarch64-linux-android24-clang 
         export CFLAGS_AARCH64_LINUX_ANDROID="--target=aarch64-linux-android --sysroot=${prebuiltPath}/sysroot" 
         export CXXFLAGS_AARCH64_LINUX_ANDROID="--target=aarch64-linux-android"
-        export BINDGEN_EXTRA_CLANG_ARGS_aarch64_linux_android="--sysroot=${prebuiltPath}/sysroot -I${prebuiltPath}/sysroot/usr/include/aarch64-linux-android" 
+        export BINDGEN_EXTRA_CLANG_ARGS_aarch64_linux_android="--sysroot=${prebuiltPath}/sysroot -I${prebuiltPath}/sysroot/usr/include/aarch64-linux-android  -I${prebuiltPath}/lib/clang/19/include" 
 
         export CC_i686_linux_android=${toolchainBinsPath}/i686-linux-android24-clang 
         export CXX_i686_linux_android=${toolchainBinsPath}/i686-linux-android24-clang++ 
@@ -68,7 +68,7 @@
         export CARGO_TARGET_I686_LINUX_ANDROID_LINKER=${toolchainBinsPath}/i686-linux-android24-clang 
         export CFLAGS_I686_LINUX_ANDROID="--target=i686-linux-android --sysroot=${prebuiltPath}/sysroot" 
         export CXXFLAGS_I686_LINUX_ANDROID="--target=i686-linux-android"
-        export BINDGEN_EXTRA_CLANG_ARGS_i686_linux_android="--sysroot=${prebuiltPath}/sysroot -I${prebuiltPath}/sysroot/usr/include/i686-linux-android"
+        export BINDGEN_EXTRA_CLANG_ARGS_i686_linux_android="--sysroot=${prebuiltPath}/sysroot -I${prebuiltPath}/sysroot/usr/include/i686-linux-android  -I${prebuiltPath}/lib/clang/19/include"
 
         export CC_x86_64_linux_android=${toolchainBinsPath}/x86_64-linux-android24-clang 
         export CXX_x86_64_linux_android=${toolchainBinsPath}/x86_64-linux-android24-clang++ 
@@ -76,7 +76,7 @@
         export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=${toolchainBinsPath}/x86_64-linux-android24-clang 
         export CFLAGS_X86_64_LINUX_ANDROID="--target=x86_64-linux-android --sysroot=${prebuiltPath}/sysroot" 
         export CXXFLAGS_X86_64_LINUX_ANDROID="--target=x86_64-linux-android"
-        export BINDGEN_EXTRA_CLANG_ARGS_x86_64_linux_android="--sysroot=${prebuiltPath}/sysroot -I${prebuiltPath}/sysroot/usr/include/x86_64-linux-android" 
+        export BINDGEN_EXTRA_CLANG_ARGS_x86_64_linux_android="--sysroot=${prebuiltPath}/sysroot -I${prebuiltPath}/sysroot/usr/include/x86_64-linux-android  -I${prebuiltPath}/lib/clang/19/include" 
 
         export CC_armv7_linux_androideabi=${toolchainBinsPath}/armv7a-linux-androideabi24-clang 
         export CXX_armv7_linux_androideabi=${toolchainBinsPath}/armv7a-linux-androideabi24-clang++ 
@@ -84,7 +84,8 @@
         export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER=${toolchainBinsPath}/armv7a-linux-androideabi24-clang 
         export CFLAGS_ARMV7_LINUX_ANDROID="--target=armv7-linux-androideabi --sysroot=${prebuiltPath}/sysroot" 
         export CXXFLAGS_ARMV7_LINUX_ANDROID="--target=armv7-linux-androideabi" 
-        export BINDGEN_EXTRA_CLANG_ARGS_armv7_linux_androideabi="--sysroot=${prebuiltPath}/sysroot -I${prebuiltPath}/sysroot/usr/include/arm-linux-androideabi" 
+        export BINDGEN_EXTRA_CLANG_ARGS_armv7_linux_androideabi="--sysroot=${prebuiltPath}/sysroot -I${prebuiltPath}/sysroot/usr/include/arm-linux-androideabi -I${prebuiltPath}/lib/clang/19/include"
+
       '';
     };
 
