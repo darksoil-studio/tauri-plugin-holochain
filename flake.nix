d1393402d9bd9ce56596c515cca181fc5c9cbb6e
guillem.cordoba <guillem.cordoba@gmail.com>
2026-02-06 18:52:52 +0100

    Updated to hc 0.6

--- flake.nix
+++ flake.nix
@@ -15,6 +15,8 @@
     scaffolding.inputs.holochain-nix-builders.follows =
       "holochain-nix-builders";
     scaffolding.inputs.holonix.follows = "holonix";
+    webkitnixpkgs.url =
+      "github:nixos/nixpkgs/ed4db9c6c75079ff3570a9e3eb6806c8f692dc26";
   };
 
   nixConfig = {
@@ -92,10 +94,10 @@
         # in pkgs.rust-bin.stable."1.85.0".minimal;
 
         dependencies.tauriApp = let
-          # pkgs = if inputs.nixpkgs.legacyPackages.${system}.stdenv.isLinux then
-          #   inputs.webkitnixpkgs.legacyPackages.${system}
-          # else
-          #   inputs.nixpkgs.legacyPackages.${system};
+          pkgs = if inputs.nixpkgs.legacyPackages.${system}.stdenv.isLinux then
+            inputs.webkitnixpkgs.legacyPackages.${system}
+          else
+            inputs.nixpkgs.legacyPackages.${system};
           buildInputs = (lib.optionals pkgs.stdenv.isLinux (with pkgs; [
             # webkitgtk # Brings libwebkit2gtk-4.0.so.37
             webkitgtk_4_1 # Needed for javascriptcoregtk
@@ -137,7 +139,8 @@
           ]));
           nativeBuildInputs = (with pkgs; [ perl pkg-config makeWrapper ])
             ++ (lib.optionals pkgs.stdenv.isLinux
-              (with pkgs; [ wrapGAppsHook3 ]))
+              # (with pkgs; [ wrapGAppsHook3 ]))
+              (with pkgs; [ wrapGAppsHook ]))
             ++ (lib.optionals pkgs.stdenv.isDarwin [ pkgs.libiconv ]);
         in { inherit buildInputs nativeBuildInputs; };
 
@@ -149,10 +152,10 @@
         };
 
         devShells.tauriDev = let
-          # pkgs = if inputs.nixpkgs.legacyPackages.${system}.stdenv.isLinux then
-          #   inputs.webkitnixpkgs.legacyPackages.${system}
-          # else
-          #   inputs.nixpkgs.legacyPackages.${system};
+          pkgs = if inputs.nixpkgs.legacyPackages.${system}.stdenv.isLinux then
+            inputs.webkitnixpkgs.legacyPackages.${system}
+          else
+            inputs.nixpkgs.legacyPackages.${system};
         in pkgs.mkShell {
           packages = with pkgs;
             [ packages.tauriRust shared-mime-info gsettings-desktop-schemas ]
