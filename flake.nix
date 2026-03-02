81439e0a7ba602b411db3d4ee69d751a1fa61632
guillem.cordoba <guillem.cordoba@gmail.com>
2026-03-02 20:40:35 +0100

    Attempt

--- flake.nix
+++ flake.nix
@@ -212,6 +212,7 @@
           packages = [ packages.holochainTauriRust ];
 
           shellHook = ''
+            ulimit -s unlimited
             export PS1='\[\033[1;34m\][tauri-plugin-holochain:\w]\$\[\033[0m\] '
             export CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUSTFLAGS='--cfg getrandom_backend="custom"'
           '';
