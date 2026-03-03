cf62630d32dae435cd095c3b935c04e17fedd36f
guillem.cordoba <guillem.cordoba@gmail.com>
2026-03-03 12:44:34 +0100

    fix android

--- nix/android.nix
+++ nix/android.nix
@@ -127,6 +127,7 @@
 
       shellHook = ''
         export PS1='\[\033[1;34m\][tauri-plugin-holochain-android:\w]\$\[\033[0m\] '
+        export CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUSTFLAGS='--cfg getrandom_backend="custom"'
       '';
     };
   };
